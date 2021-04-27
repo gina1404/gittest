c1fbde544713d2ebbfc1bc8668f7fee0fee72a5f
gina1 <gina1@DESKTOP-V5IFIJ6>
2021-04-27 19:25:43 +0900

    #01 main에 출력문 추가 -j-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,11 +1,9 @@
 package com.git.main;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		int su=4;
-		Scanner sc=new Scanner(System.in);
+		System.out.println("난 BS브런치야!");
+		
 	}
 }
